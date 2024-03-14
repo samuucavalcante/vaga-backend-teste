33076945a086774c722c81481f86aa9392766589
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2024-03-13 22:55:42 -0300

    feat: setup port on server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import { dotenv } from "./config/dotenv";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen();
+app.listen(dotenv.BACKEND_PORT, () =>
+  console.log(`Server is Running on Port ${dotenv.BACKEND_PORT}`),
+);
